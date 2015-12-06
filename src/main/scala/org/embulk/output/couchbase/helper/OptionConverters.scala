a277749dcf0a3ce408d599ef82368cde7a567161
zaneli <shun.otani@gmail.com>
2015-12-06 15:36:02 +0900

    com.google.common.base.Optional to scala.Option Convertor

---
+++ src/main/scala/org/embulk/output/couchbase/helper/OptionConverters.scala
@@ -0,0 +1,13 @@
+package org.embulk.output.couchbase.helper
+
+import com.google.common.base.Optional
+
+object OptionConverters {
+  import scala.language.implicitConversions
+
+  implicit class Optional2Option[A](val underlying: Optional[A]) extends AnyVal {
+    def asScala: Option[A] = PartialFunction.condOpt(underlying) {
+      case o if o.isPresent => o.get()
+    }
+  }
+}
